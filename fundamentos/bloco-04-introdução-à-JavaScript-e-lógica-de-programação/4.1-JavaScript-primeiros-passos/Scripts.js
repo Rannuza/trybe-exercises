aa27cf0ed48bc8e879cd449ac9fae290e5c21b04
Rannah Lima <rannuza@gmail.com>
2021-11-05 16:15:33 -0300

    Primeiro exercício de variáveis

---
+++ fundamentos/bloco-04-introdução-à-JavaScript-e-lógica-de-programação/4.1-JavaScript-primeiros-passos/Scripts.js
@@ -0,0 +1,13 @@
+let age = 20;
+let myName = "Rannah Lima";
+let birthCity = "Recife";
+let birthYear = 1992;
+
+age = 28;
+birthYear = 2030;
+birthCity = "Paris"
+
+console.log(age);
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear) 
