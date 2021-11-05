78142c8b218307a3beff20610f50351b917d5dc4
Rannah Lima <rannuza@gmail.com>
2021-11-05 17:55:53 -0300

    Atividade do conteúdo de Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas

--- fundamentos/bloco-04-introdução-à-JavaScript-e-lógica-de-programação/4.1-JavaScript-primeiros-passos/Scripts.js
+++ fundamentos/bloco-04-introdução-à-JavaScript-e-lógica-de-programação/4.1-JavaScript-primeiros-passos/Scripts.js
@@ -1,4 +1,4 @@
-let age = 20;
+/*let age = 20;
 let myName = "Rannah Lima";
 let birthCity = "Recife";
 let birthYear = 1992;
@@ -10,4 +10,27 @@ birthCity = "Paris"
 console.log(age);
 console.log(myName);
 console.log(birthCity);
-console.log(birthYear) 
+console.log(birthYear) */
+
+/*let patientId = "50";
+let isEnrolled = true;
+const patientInfo = {
+    firstName: 'Ana',
+    lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+//log(typeof patientAge);*/
+
+const base = 5;
+let height = 8;
+const area = (base * height)
+
+const perimeter = 2 * (base + height)
+
+console.log(area);
+console.log(perimeter);
