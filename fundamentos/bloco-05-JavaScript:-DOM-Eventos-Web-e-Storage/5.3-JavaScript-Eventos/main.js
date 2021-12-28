e8805ebb058e89c7b5cf3e41e262db8afbe3bc69
Rannah Lima <rannuza@gmail.com>
2021-12-28 20:48:23 -0300

    Corrige e finaliza o exercício da parte II do dia 5.2

--- fundamentos/bloco-05-JavaScript:-DOM-Eventos-Web-e-Storage/5.3-JavaScript-Eventos/main.js
+++ fundamentos/bloco-05-JavaScript:-DOM-Eventos-Web-e-Storage/5.3-JavaScript-Eventos/main.js
@@ -5,32 +5,6 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 
 
-secondLi.addEventListener('click', addClassTech);
-thirdLi.addEventListener('click', addClassTech);
-
-function addClassTech (event) {
-
-  event.target.classList.add('tech');
-}
-
-input.addEventListener('input', alteraTexto)
-
-function alteraTexto (event) {
-
-  firstLi.innerText = input.value; 
-}
-
-myWebpage.addEventListener('dblclick', redirecionar)
-myWebpage.addEventListener('mouseover', mudaCor)
-
-function redirecionar (event) {
-  myWebpage = window.open("https://pt.stackoverflow.com/questions/3312/como-redirecionar-o-usu%C3%A1rio-para-outra-p%C3%A1gina-em-javascript-jquery");
-  
-}
-function mudaCor (event) {
-  myWebpage.style.color = "red";
-}
-
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
@@ -38,16 +12,45 @@ function mudaCor (event) {
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
 
-// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-// redirecione para alguma página;
+function addClassTech (event) {
+  const classe = document.querySelector('.tech');
+  classe.classList.remove('tech');
+  event.target.classList.add('tech');
+}
+
+firstLi.addEventListener('click', addClassTech);
+secondLi.addEventListener('click', addClassTech);
+thirdLi.addEventListener('click', addClassTech);
+
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
+
+input.addEventListener('input', alteraTexto)
+
+function alteraTexto (event) {
+  const tech = document.querySelector('.tech');
+  tech.innerText = input.value; 
+}
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
-// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-// a cor do mesmo;
+function redirecionar (event) {
+  myWebpage = window.open("https://rannuza.github.io/");
+};
 
+myWebpage.addEventListener('dblclick', redirecionar);
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
+
+function mudaCor (event) {
+  myWebpage.style.color = "red";
+}
+function mudaCor2 (event) {
+  myWebpage.style.color = "white";
+}
+
+myWebpage.addEventListener('mouseover', mudaCor);
+myWebpage.addEventListener('mouseleave', mudaCor2);
 // Segue abaixo um exemplo do uso de event.target:
 
 
